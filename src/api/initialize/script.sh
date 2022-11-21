657a38682b353a71bfbafb7d7085fa5cacf01c37
Toni Verbeiren <toni.verbeiren@gmail.com>
2022-11-21 09:32:18 +0100

    Make db and db_version parameters

--- src/api/initialize/script.sh
+++ src/api/initialize/script.sh
@@ -4,8 +4,17 @@
 par_endpoint="http://127.0.0.1:8090"
 par_classpath="com.dataintuitive.luciusapi"
 par_application="lucius"
-par_conf="etc/initialize.conf"
+par_conf="etc/api.conf"
+par_db="my_test_db"
+par_db_version=10
 ## VIASH END
 
-curl -s --data-binary "@${par_conf}" \
+tmpfile=`mktemp`
+
+cp "$par_conf" "$tmpfile"
+
+gsed -i "s#__template_uri__#$par_db#" "$tmpfile"
+gsed -i "s#db\.version = .*#db.version = $par_db_version#" "$tmpfile"
+
+curl -s --data-binary "@${tmpfile}" \
   "${par_endpoint}/jobs?context=${par_application}&appName=${par_application}&classPath=${par_classpath}.initialize"
