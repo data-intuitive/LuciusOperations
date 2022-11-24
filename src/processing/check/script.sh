eec89c1f944ce64760bee40c7de9b49197743445
Toni Verbeiren <toni.verbeiren@gmail.com>
2022-11-24 12:03:36 +0100

    Add processing/check component

--- src/processing/check/script.sh
+++ src/processing/check/script.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+set -e
 
 ## VIASH START
 par_endpoint="http://127.0.0.1:8090"
-par_classpath="com.dataintuitive.luciusapi"
-par_application="lucius"
+par_classpath="com.dataintuitive.luciusprocessing"
+par_conf="etc/processing.conf"
+par_db="my_test_db"
+par_db_version=10
+par_application=luciusprocessing
+par_cutoff="2022-12-01"
+par_geneAnnotations=""
+par_incremental=false
 ## VIASH END
 
-curl -s -X POST \
-  "${par_endpoint}/jobs?context=${par_application}&appName=${par_application}&classPath=${par_classpath}.statistics&sync=true"
+tmpfile=`mktemp`
+cp "$par_conf" "$tmpfile"
+
+my_sed="sed -i'.original'"
+
+$my_sed "s#__input__#$par_input#" "$tmpfile"
+$my_sed "s#__db__#$par_db#" "$tmpfile"
+$my_sed "s#__cutoff__#$par_cutoff#" "$tmpfile"
+$my_sed "s#__incremental__#$par_incremental#" "$tmpfile"
+
+echo ">> Effective config used:"
+cat $tmpfile
+
+echo ">> Run command"
+curl -s --data-binary "@${tmpfile}" \
+  "${par_endpoint}/jobs?context=${par_application}&appName=${par_application}&classPath=${par_classpath}.check&sync=true"
