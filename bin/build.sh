67749fdfc97689c8b5a179c0b0da29efb6a6105c
Toni Verbeiren <toni.verbeiren@gmail.com>
2022-11-25 16:31:46 +0100

    bin/build.sh convenience script

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+echo
+echo ">> Building both namespaces"
+$REPO_ROOT/bin/viash ns build -l -f -n processing -t utils/processing
+$REPO_ROOT/bin/viash ns build -l -f -n api -t utils/api
+
+echo
+echo ">> Please see under utils/ to find the tools for api and processing..."
+echo
