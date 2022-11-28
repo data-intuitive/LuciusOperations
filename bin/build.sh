399619fc8042c6bf7f2967f7025b02824bc29160
Toni Verbeiren <toni.verbeiren@gmail.com>
2022-11-28 16:18:27 +0100

    Avoid parallel build

--- bin/build.sh
+++ bin/build.sh
@@ -5,8 +5,8 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 echo
 echo ">> Building both namespaces"
-$REPO_ROOT/bin/viash ns build -l -f -n processing -t utils/processing
-$REPO_ROOT/bin/viash ns build -l -f -n api -t utils/api
+$REPO_ROOT/bin/viash ns build -f -n processing -t utils/processing
+$REPO_ROOT/bin/viash ns build -f -n api -t utils/api
 
 echo
 echo ">> Please see under utils/ to find the tools for api and processing..."
